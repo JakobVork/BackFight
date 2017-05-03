3c532ad8755e9c4abe6fab15059c1c998dd5603e
Anders Meidahl Münsberg <Andersmeidahl@gmail.com>
2017-05-03 19:06:56 +0200

    Refactored the Tile Class to have a bool to detect if it CanBePassed, to use for checking when generating the grid. Renamed more variables to better readability

--- BackFight/app/src/main/java/com/studio/jarn/backfight/GameActivity.java
+++ BackFight/app/src/main/java/com/studio/jarn/backfight/GameActivity.java
@@ -14,11 +14,11 @@ public class GameActivity extends Activity
 {
     private static final int GridSizeWidthAndHeight = 16;
     private static final int SquaresViewedAtStartup = 3;
-    private final Tile wallTile = new Tile(Tile.Types.Wall, null, 0);
-    private final Tile floorTile = new Tile(Tile.Types.WoodenFloor, null, 0);
+    private final Tile wallTile = new Tile(Tile.Types.Wall, null);
+    private final Tile floorTile = new Tile(Tile.Types.WoodenFloor, null);
     private final List<Integer> checkedList = new ArrayList<>();
     private Tile[][] mGrid;
-    private int visitCounter = 0;
+    private int TileConnectivityCollectionNrCounter = 0;
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +48,7 @@ public class GameActivity extends Activity
         players.add(new Player((BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.player32)), "Pernille"));
 
 
-        Tile floorTileWithPlayers = new Tile(Tile.Types.WoodenFloor, players, 0);
+        Tile floorTileWithPlayers = new Tile(Tile.Types.WoodenFloor, players);
     }
 
     private void setupMyGrid(GridType gridType)
@@ -91,13 +91,13 @@ public class GameActivity extends Activity
                 for (int c = 0; c < GridSizeWidthAndHeight; c++) {
 
                     //check if tile is floor and if it has already been visited
-                    if (mGrid[r][c].Type == Tile.Types.WoodenFloor && checkIfVisited(mGrid[r][c].Visit)) {
+                    if (mGrid[r][c].CanBePassed && checkIfVisited(mGrid[r][c].TileConnectivityCollectionNr)) {
                         changeWallToFloor(r, c);
                     }
                 }
             }
-            mGrid[1][1].Visit = 0;
-            visitCounter = 0;
+            //reset TileConnectivityCollectionNrCounter and clear the checkedList so its ready for the next checkConnectivity() run
+            TileConnectivityCollectionNrCounter = 0;
             checkedList.clear();
         }
     }
@@ -105,23 +105,23 @@ public class GameActivity extends Activity
     private void changeWallToFloor(int r, int c) {
         //check if it is first row and then change the first wall it meets going to the right to a floor
         if (r == 0) {
-            for (int counter = 0; counter < GridSizeWidthAndHeight; counter++) {
-                if (mGrid[r + counter][c].Type == Tile.Types.Wall) {
-                    mGrid[r + counter][c] = floorTile;
+            for (int spacesToGoRight = 0; spacesToGoRight < GridSizeWidthAndHeight; spacesToGoRight++) {
+                if (!mGrid[r + spacesToGoRight][c].CanBePassed) {
+                    mGrid[r + spacesToGoRight][c] = floorTile;
                     break;
                 }
             }
         } else {
             outerLoop:
-            for (int counter = 0; counter <= r; counter++) {
-                if (mGrid[r - counter][c].Type == Tile.Types.Wall) { //Try to change wall tile to floor, to the left side
-                    mGrid[r - counter][c] = floorTile;
+            for (int spacesToGoLeft = 0; spacesToGoLeft <= r; spacesToGoLeft++) {
+                if (!mGrid[r - spacesToGoLeft][c].CanBePassed) { //Try to change wall tile to floor, to the left side
+                    mGrid[r - spacesToGoLeft][c] = floorTile;
                     break;
                 }
-                if (counter == r) { //if there is no wall tiles to the left all the way to the edge, try to change to the right
-                    for (int counter2 = 0; counter2 <= GridSizeWidthAndHeight - 1 - r; counter2++) {
-                        if (mGrid[r + counter2][c].Type == Tile.Types.Wall) {
-                            mGrid[r + counter2][c] = floorTile;
+                if (spacesToGoLeft == r) { //if there is no wall tiles to the left all the way to the edge, try to change to the right
+                    for (int spacesToGoRight = 0; spacesToGoRight <= GridSizeWidthAndHeight - 1 - r; spacesToGoRight++) {
+                        if (!mGrid[r + spacesToGoRight][c].CanBePassed) {
+                            mGrid[r + spacesToGoRight][c] = floorTile;
                             break outerLoop;
                         }
                     }
@@ -131,14 +131,14 @@ public class GameActivity extends Activity
     }
 
     // Check if the tile group has already been visited (part of the checkedList)
-    private boolean checkIfVisited(int visit) {
+    private boolean checkIfVisited(int tileConnectivityCollectionNr) {
 
         for (Integer checkedInts : checkedList) {
-            if (checkedInts == visit) {
+            if (checkedInts == tileConnectivityCollectionNr) {
                 return false;
             }
         }
-        checkedList.add(visit);
+        checkedList.add(tileConnectivityCollectionNr);
         return true;
     }
 
@@ -146,34 +146,34 @@ public class GameActivity extends Activity
         //reset the visit count for all spaces in the Grid
         for (int i = 0; i < GridSizeWidthAndHeight; i++) {
             for (int j = 0; j < GridSizeWidthAndHeight; j++) {
-                mGrid[i][j].Visit = 0;
+                mGrid[i][j].TileConnectivityCollectionNr = 0;
             }
         }
 
         //for each floor tile go and visit all their neighbors, when no more neighbors count
-        // visitCounter one up and find the next floor tile that has not been visited.
+        // TileConnectivityCollectionNrCounter one up and find the next floor tile that has not been visited.
         for (int i = 0; i < GridSizeWidthAndHeight; i++) {
             for (int j = 0; j < GridSizeWidthAndHeight; j++) {
-                if (mGrid[i][j].Type == Tile.Types.WoodenFloor && mGrid[i][j].Visit == 0) {
-                    visitAllConnectedNeighbors(++visitCounter, i, j);
+                if (mGrid[i][j].CanBePassed && mGrid[i][j].TileConnectivityCollectionNr == 0) {
+                    visitAllConnectedNeighbors(++TileConnectivityCollectionNrCounter, i, j);
                 }
             }
         }
-        return visitCounter;
+        return TileConnectivityCollectionNrCounter;
     }
 
     //recursive logic to take a floor tile and visit all the connecting neighbors
-    private void visitAllConnectedNeighbors(int visitCollectionNr, int i, int j) {
-        Tile floorTile = new Tile(Tile.Types.WoodenFloor, null, visitCollectionNr);
+    private void visitAllConnectedNeighbors(int tileConnectivityCollectionNr, int i, int j) {
+        Tile floorTile = new Tile(Tile.Types.WoodenFloor, null, tileConnectivityCollectionNr);
         mGrid[i][j] = floorTile;
-        if (i > 0 && mGrid[i - 1][j].Type == Tile.Types.WoodenFloor && mGrid[i - 1][j].Visit != visitCollectionNr)
-            visitAllConnectedNeighbors(visitCollectionNr, i - 1, j);
-        if (i < GridSizeWidthAndHeight - 1 && mGrid[i + 1][j].Type == Tile.Types.WoodenFloor && mGrid[i + 1][j].Visit != visitCollectionNr)
-            visitAllConnectedNeighbors(visitCollectionNr, i + 1, j);
-        if (j > 0 && mGrid[i][j - 1].Type == Tile.Types.WoodenFloor && mGrid[i][j - 1].Visit != visitCollectionNr)
-            visitAllConnectedNeighbors(visitCollectionNr, i, j - 1);
-        if (j < GridSizeWidthAndHeight - 1 && mGrid[i][j + 1].Type == Tile.Types.WoodenFloor && mGrid[i][j + 1].Visit != visitCollectionNr)
-            visitAllConnectedNeighbors(visitCollectionNr, i, j + 1);
+        if (i > 0 && mGrid[i - 1][j].CanBePassed && mGrid[i - 1][j].TileConnectivityCollectionNr != tileConnectivityCollectionNr)
+            visitAllConnectedNeighbors(tileConnectivityCollectionNr, i - 1, j);
+        if (i < GridSizeWidthAndHeight - 1 && mGrid[i + 1][j].CanBePassed && mGrid[i + 1][j].TileConnectivityCollectionNr != tileConnectivityCollectionNr)
+            visitAllConnectedNeighbors(tileConnectivityCollectionNr, i + 1, j);
+        if (j > 0 && mGrid[i][j - 1].CanBePassed && mGrid[i][j - 1].TileConnectivityCollectionNr != tileConnectivityCollectionNr)
+            visitAllConnectedNeighbors(tileConnectivityCollectionNr, i, j - 1);
+        if (j < GridSizeWidthAndHeight - 1 && mGrid[i][j + 1].CanBePassed && mGrid[i][j + 1].TileConnectivityCollectionNr != tileConnectivityCollectionNr)
+            visitAllConnectedNeighbors(tileConnectivityCollectionNr, i, j + 1);
     }
 
 
@@ -208,7 +208,7 @@ public class GameActivity extends Activity
     private void recursionMaze(int r, int c) {
         // 4 random directions
         int[] randDirs = generateRandomDirections();
-        Tile floorTile = new Tile(Tile.Types.WoodenFloor, null, 0);
+        Tile floorTile = new Tile(Tile.Types.WoodenFloor, null);
         // Examine each direction
         for (int randDir : randDirs) {
 
@@ -217,7 +217,7 @@ public class GameActivity extends Activity
                     //　Whether 2 cells up is out or not
                     if (r - 2 <= 0)
                         continue;
-                    if (mGrid[r - 2][c].Type != Tile.Types.WoodenFloor) {
+                    if (!mGrid[r - 2][c].CanBePassed) {
                         mGrid[r - 2][c] = floorTile;
                         mGrid[r - 1][c] = floorTile;
                         recursionMaze(r - 2, c);
@@ -227,7 +227,7 @@ public class GameActivity extends Activity
                     // Whether 2 cells to the right is out or not
                     if (c + 2 >= GridSizeWidthAndHeight - 1)
                         continue;
-                    if (mGrid[r][c + 2].Type != Tile.Types.WoodenFloor) {
+                    if (!mGrid[r][c + 2].CanBePassed) {
                         mGrid[r][c + 2] = floorTile;
                         mGrid[r][c + 1] = floorTile;
                         recursionMaze(r, c + 2);
@@ -237,7 +237,7 @@ public class GameActivity extends Activity
                     // Whether 2 cells down is out or not
                     if (r + 2 >= GridSizeWidthAndHeight - 1)
                         continue;
-                    if (mGrid[r + 2][c].Type != Tile.Types.WoodenFloor) {
+                    if (!mGrid[r + 2][c].CanBePassed) {
                         mGrid[r + 2][c] = floorTile;
                         mGrid[r + 1][c] = floorTile;
                         recursionMaze(r + 2, c);
@@ -247,7 +247,7 @@ public class GameActivity extends Activity
                     // Whether 2 cells to the left is out or not
                     if (c - 2 <= 0)
                         continue;
-                    if (mGrid[r][c - 2].Type != Tile.Types.WoodenFloor) {
+                    if (!mGrid[r][c - 2].CanBePassed) {
                         mGrid[r][c - 2] = floorTile;
                         mGrid[r][c - 1] = floorTile;
                         recursionMaze(r, c - 2);
@@ -255,7 +255,6 @@ public class GameActivity extends Activity
                     break;
             }
         }
-
     }
 
     //inspiration for the maze function have been taken from http://www.migapro.com/depth-first-search/
--- BackFight/app/src/main/java/com/studio/jarn/backfight/Tile.java
+++ BackFight/app/src/main/java/com/studio/jarn/backfight/Tile.java
@@ -6,13 +6,37 @@ import java.util.List;
 class Tile {
     Types Type;
     List<Player> Players;
-    int Visit;
+    int TileConnectivityCollectionNr;
+    boolean CanBePassed;
 
 
-    Tile(Types type, List<Player> players, int visit) {
+    Tile(Types type, List<Player> players, int tileConnectivityCollectionNr) {
         Type = type;
         Players = players;
-        Visit = visit;
+
+        //Used for calculating connectivity for the CanBePassed tiles
+        TileConnectivityCollectionNr = tileConnectivityCollectionNr;
+
+
+        canBePassedSwitch(type);
+    }
+
+    Tile(Types type, List<Player> players) {
+        this(type, players, 0);
+    }
+
+
+    private void canBePassedSwitch(Types type) {
+        switch (type) {
+            case WoodenFloor: {
+                CanBePassed = true;
+                break;
+            }
+            case Wall: {
+                CanBePassed = false;
+                break;
+            }
+        }
     }
 
     enum Types {
