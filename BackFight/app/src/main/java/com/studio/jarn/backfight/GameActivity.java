b47610c5dc295596dc3cafafa077a1b5c098f3b0
Nicklas Nielsen <nnicklas92@gmail.com>
2017-05-04 16:22:27 +0200

    Made changes for selecting a tile and added the two bitmaps for the selected figure

--- BackFight/app/src/main/java/com/studio/jarn/backfight/GameActivity.java
+++ BackFight/app/src/main/java/com/studio/jarn/backfight/GameActivity.java
@@ -1,7 +1,6 @@
 package com.studio.jarn.backfight;
 
 import android.app.Activity;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -11,9 +10,8 @@ public class GameActivity extends Activity
 {
     static public final int GridSizeWidthAndHeight = 5;
     static public final int SquaresViewedAtStartup = 3;
-
+    List<Player> players = new ArrayList<>();
     private Tile[][] mGrid;
-
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,16 +31,19 @@ public class GameActivity extends Activity
     public void setupMyGrid (int n)
     {
         /*mGrid = new Tile [n] [n];*/
-        List<Player> players = new ArrayList<>();
-        players.add(new Player((BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.point)), "Anders"));
-        players.add(new Player((BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.player32)), "Pernille"));
-        players.add(new Player((BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.cart)), "Pernille"));
-        players.add(new Player((BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.point)), "Pernille"));
-        players.add(new Player((BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.player32)), "Pernille"));
+        players.add(new Player(R.drawable.player32, "Pernille"));
+        players.add(new Player(R.drawable.point, "Anders"));
+        players.add(new Player(R.drawable.cart, "Pernille"));
+        players.add(new Player(R.drawable.point, "Pernille"));
+        players.add(new Player(R.drawable.player32, "Pernille"));
 
         Tile[] arrayWithPlayers = new Tile[]{new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.Wall, players)};
-        Tile[] array = new Tile[]{new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.Wall, null)};
+        Tile[] array1 = new Tile[]{new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.Wall, null)};
+        Tile[] array2 = new Tile[]{new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.Wall, null)};
+        Tile[] array3 = new Tile[]{new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.Wall, null)};
+        Tile[] array4 = new Tile[]{new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.WoodenFloor, null), new Tile(Tile.Types.Wall, null), new Tile(Tile.Types.Wall, null)};
 
-        mGrid = new Tile[][]{array, array, array, array, arrayWithPlayers};
+
+        mGrid = new Tile[][]{array1, array2, array3, array4, arrayWithPlayers};
     }
 }
